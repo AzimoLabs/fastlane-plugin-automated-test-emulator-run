a25c89513e327658dd0318e4e66640f4c1cdf3c5
Daniel Hartwich <daniel.hartwich@xing.com>
2017-09-06 10:19:50 +0200

    Include logging for parameters

--- lib/fastlane/plugin/automated_test_emulator_run_xing/actions/automated_test_emulator_run_xing_action.rb
+++ lib/fastlane/plugin/automated_test_emulator_run_xing/actions/automated_test_emulator_run_xing_action.rb
@@ -190,7 +190,7 @@ module Fastlane
               gradle = Helper::GradleHelper.new(gradle_path: Dir["./gradlew"].last)
 
               UI.message("Using spoon task.".green)
-              puts params[:gradle_flags]
+              puts params[:gradle_task]
               ports = Array.new
               spoon_devices = ""
               for i in 0...avd_schemes.length
--- lib/fastlane/plugin/automated_test_emulator_run_xing/version.rb
+++ lib/fastlane/plugin/automated_test_emulator_run_xing/version.rb
@@ -1,5 +1,5 @@
 module Fastlane
   module AutomatedTestEmulatorRunXing
-    VERSION = "1.0.4"
+    VERSION = "1.0.5"
   end
 end
