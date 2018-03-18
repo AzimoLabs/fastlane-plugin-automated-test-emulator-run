53723d9117f6920d9673779a9108e3992c9a8694
Kyrylo Mikos <kiril.mik.os@gmail.com>
2018-03-18 21:09:47 +0200

    Use serial parameter correctly and pass list of serials from json.

--- lib/fastlane/plugin/automated_test_emulator_run/actions/automated_test_emulator_run_action.rb
+++ lib/fastlane/plugin/automated_test_emulator_run/actions/automated_test_emulator_run_action.rb
@@ -187,9 +187,10 @@ module Fastlane
 
             unless gradle_task.nil?
               gradle = Helper::GradleHelper.new(gradle_path: Dir["./gradlew"].last)
+              serial = avd_schemes.map { |x| "emulator-" + x.launch_avd_port }.join(",")
 
               UI.message("Using gradle task.".green)
-              gradle.trigger(task: params[:gradle_task], flags: params[:gradle_flags], serial: nil)
+              gradle.trigger(task: params[:gradle_task], flags: params[:gradle_flags], serial: serial)
             end
           ensure 
             # Clean up
