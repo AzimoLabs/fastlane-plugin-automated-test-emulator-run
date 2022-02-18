aa283884fee8b163687e34a41a035a461c818eaa
Hiroto Nakamura <hiroto.nakamura95@gmail.com>
2022-02-18 17:06:30 +1300

    Add support for cmdline-tools

--- lib/fastlane/plugin/automated_test_emulator_run.rb
+++ lib/fastlane/plugin/automated_test_emulator_run.rb
@@ -3,7 +3,7 @@ require 'fastlane/plugin/automated_test_emulator_run/version'
 module Fastlane
   module AutomatedTestEmulatorRun
     def self.all_classes
-      Dir[File.expand_path('**/{actions,factory,provider}/*.rb', File.dirname(__FILE__))]
+      Dir[File.expand_path('**/{actions,factory,provider,helper}/*.rb', File.dirname(__FILE__))]
     end
   end
 end
